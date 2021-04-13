ace84b88d0c927a1f0d187b36296ee298297c757
Miguel Jimenez <miguel1315@gmail.com>
2021-04-13 12:48:41 +0200

    Fix setup.py to upload to pypi and MANIFEST

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name="dataclass-bakery",
-    packages=["dataclass-bakery"],
+    packages=["dataclass_bakery"],
     version="0.1",
     description="Dataclass Bakery offers you a smart way to create fixtures for testing in Python with dataclasses",
     author="Miguel Jiménez",

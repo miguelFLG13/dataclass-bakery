608bbf6b4f52c8fa08c96a35718c87bf942a5a1d
Miguel Jimenez <miguel1315@gmail.com>
2021-04-13 12:33:23 +0200

    Add baker

---
+++ dataclass_bakery/baker.py
@@ -0,0 +1,24 @@
+from typing import Any, Dict, List, Optional, Union
+
+from dataclass_bakery.generators.random_data_class_generator import (
+    RandomDataClassGenerator,
+)
+
+
+def make(
+    _data_class: Any,
+    _quantity: Optional[int] = 1,
+    _attr_defaults: Optional[Dict] = None,
+    **attrs: Any
+) -> Union[List, Any]:
+    random_data_class_generator = RandomDataClassGenerator()
+
+    data_class_objects = []
+    for _ in range(_quantity):
+        data_class_object = random_data_class_generator.generate(_data_class)
+        data_class_objects.append(data_class_object)
+
+    if _quantity > 1:
+        return data_class_objects
+
+    return data_class_object

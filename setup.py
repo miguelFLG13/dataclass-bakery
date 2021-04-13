fbbf998e58357fd7ecaf3a4114cc0da1e9dbae71
Miguel Jimenez <miguel1315@gmail.com>
2021-04-13 12:33:40 +0200

    Add pypi files

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+
+setup(
+    name="dataclass-bakery",
+    packages=["dataclass-bakery"],
+    version="0.1",
+    description="Dataclass Bakery offers you a smart way to create fixtures for testing in Python with dataclasses",
+    author="Miguel Jiménez",
+    author_email="miguelflg13@gmx.com",
+    url="https://github.com/miguelFLG13/dataclass-bakery",
+    download_url="https://github.com/miguelFLG13/dataclass-bakery/tarball/0.1",
+    keywords=["testing", "dataclass", "bakery", "fixture"],
+    classifiers=[],
+)

c9dec5d2e46e6d24d81cdad8f46c2ea89658b095
Miguel Jimenez <miguel1315@gmail.com>
2021-04-19 12:24:47 +0200

    Improve baker to get custom attributes of the objects

--- src/dataclass_bakery/baker.py
+++ src/dataclass_bakery/baker.py
@@ -8,14 +8,15 @@ from dataclass_bakery.generators.random_data_class_generator import (
 def make(
     _data_class: Any,
     _quantity: Optional[int] = 1,
-    _attr_defaults: Optional[Dict] = None,
-    **attrs: Any
+    _attr_defaults: Optional[Dict] = {},
 ) -> Union[List, Any]:
     random_data_class_generator = RandomDataClassGenerator()
 
     data_class_objects = []
     for _ in range(_quantity):
-        data_class_object = random_data_class_generator.generate(_data_class)
+        data_class_object = random_data_class_generator.generate(
+            _data_class, **_attr_defaults
+        )
         data_class_objects.append(data_class_object)
 
     if _quantity > 1:

413b5b88ced200e4208e9a25edf520bfc7c31ca5
Miguel Jimenez <miguel1315@gmail.com>
2021-04-26 16:36:50 +0200

    Fix documentation to upload to readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = "2021, Miguel Jiménez"
 author = "Miguel Jiménez"
 
 # The full version, including alpha/beta/rc tags
-release = "1.0.0"
+release = "0.0.6"
 
 
 # -- General configuration ---------------------------------------------------
